149806e331204dc9d2673a133d7eeecf42b59daf
johnpascual <ashleypascual@hotmail.es>
2022-07-17 23:11:47 +0100

    created my first ejs template!

--- app.js
+++ app.js
@@ -2,19 +2,46 @@ const express = require("express")
 const bodyParser = require("body-parser")
 
 const app = express()
+app.set('view engine', 'ejs') // Tells our app to use EJS as its view engine
 
 app.get("/", (req, res) => {
 
     var today = new Date()
     var currentDay = today.getDay()
+    var day = ""
 
-    if (currentDay === 6 || currentDay === 0){
-        res.sendFile(__dirname + "/index.html")
-    } else {
-        res.write("<h1>Boo! I have to work!</h1>")
+    switch (currentDay) {
+        case 0:
+            day = "Sunday"
+            break;
+        case 1:
+            day = "Monday"
+            break;
+        case 2:
+            day = "Tuesday"
+            break;
+        case 3:
+            day = "Wednesday"
+            break;
+        case 4:
+            day = "Thursday"
+            break;
+        case 5:
+            day = "Friday"
+            break;
+        case 6:
+            day = "Saturday"
+            break;
+        case 7:
+            day = "Sunday"
+            break;
+        default:
+            console.log("Error")
+            break;
     }
-        
-    
+
+    res.render("list", {kindOfDay: day})
+
 })
 
 app.listen(3000, () => {
