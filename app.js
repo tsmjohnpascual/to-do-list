cb00f1d36a1571f44e946afbd8492220c82a316e
johnpascual <ashleypascual@hotmail.es>
2022-07-17 23:52:25 +0100

    adding items to list

--- app.js
+++ app.js
@@ -2,46 +2,32 @@ const express = require("express")
 const bodyParser = require("body-parser")
 
 const app = express()
+var items = ["Buy Food", "Cook food", "Eat food"]
 app.set('view engine', 'ejs') // Tells our app to use EJS as its view engine
+app.use(bodyParser.urlencoded({extended: true}))
+
+
 
 app.get("/", (req, res) => {
 
     var today = new Date()
-    var currentDay = today.getDay()
-    var day = ""
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday"
-            break;
-        case 1:
-            day = "Monday"
-            break;
-        case 2:
-            day = "Tuesday"
-            break;
-        case 3:
-            day = "Wednesday"
-            break;
-        case 4:
-            day = "Thursday"
-            break;
-        case 5:
-            day = "Friday"
-            break;
-        case 6:
-            day = "Saturday"
-            break;
-        case 7:
-            day = "Sunday"
-            break;
-        default:
-            console.log("Error")
-            break;
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
     }
 
-    res.render("list", {kindOfDay: day})
+    var day = today.toLocaleDateString("en-UK", options)
 
+    res.render("list", {kindOfDay: day, newListItems: items})
+
+})
+
+app.post("/", (req, res) => {
+    var item = req.body.newItem
+    items.push(item)
+    res.redirect("/")
 })
 
 app.listen(3000, () => {
